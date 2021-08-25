9578fdfeb0339318e70c2aa5288754a4a0fb2db3
Tulyakai <tulyawatt@gmail.com>
2021-08-26 01:17:51 +0700

    3

--- src/main/java/se331/lab/rest/controller/EventController.java
+++ src/main/java/se331/lab/rest/controller/EventController.java
@@ -1,8 +1,12 @@
 package se331.lab.rest.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 import se331.lab.rest.entity.Event;
 
 import javax.annotation.PostConstruct;
@@ -90,7 +94,35 @@ public class EventController {
         );
     }
     @GetMapping("events")
-    public ResponseEntity<?> getEventLists(){
-        return ResponseEntity.ok(eventList);
+    public ResponseEntity<?> getEventLists(@RequestParam(value = "_limit", required = false) Integer perPage, @RequestParam(value = "_page", required = false)Integer page){
+        perPage = perPage == null?eventList.size():perPage;
+        page = page == null?1:page;
+        Integer firstIndex = (page-1)*perPage;
+        List<Event> output = new ArrayList<>();
+
+        try{
+            for (int i = firstIndex; i<firstIndex+ perPage; i++){
+                output.add(eventList.get(i));
+            }
+        }catch (IndexOutOfBoundsException err){
+            return ResponseEntity.ok(output);
+        }
+        return ResponseEntity.ok(output);
+    }
+
+    @GetMapping("events/{id}")
+    public ResponseEntity<?> getEvent(@PathVariable("id") Long id){
+        Event output = null;
+        for (Event event: eventList){
+            if (event.getId().equals(id)){
+                output = event;
+                break;
+            }
+        }
+        if (output != null){
+            return ResponseEntity.ok(output);
+        }else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "The given id is not found");
+        }
     }
 }
